d1f6270a72cb4f6afa7a1788a9c9c225c8e59f7b
Yikuan Sun <yikuansun2015@gmail.com>
2021-02-07 20:29:04 -0500

    added some shine to images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from PIL import Image, ImageStat, ImageEnhance
+from PIL import Image, ImageStat, ImageEnhance, ImageFilter
+from blend_modes import *
+import numpy
 
 def smart_darken(img_obj):
     # get avg pixel brightness
@@ -15,7 +17,14 @@ def smart_darken(img_obj):
         img_obj = contrast_enhancer.enhance(factor)
     return img_obj
 
+def soft_glow(img_obj):
+    light_blurred = img_obj.filter(ImageFilter.GaussianBlur(100))
+    top_layer = numpy.array(light_blurred).astype(float)
+    bottom_layer = numpy.array(img_obj).astype(float)
+    return Image.fromarray( numpy.uint8( lighten_only(bottom_layer, top_layer, 0.25) ) )
+
 if __name__ == "__main__":
-    input_image = Image.open("input.jpg")
+    input_image = Image.open("input.jpg").convert("RGBA")
     input_image = smart_darken(input_image)
+    input_image = soft_glow(input_image)
     input_image.show()
